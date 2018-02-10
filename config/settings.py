a6eea2612ba5183d36b948b397dba2686d524cfb
Harinarayan Sreenivasan <hsreenivasan@paypal.com>
2018-02-10 16:57:33 +0530

    config module

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+# Global configuration settings.
+# They are nothing but python variables that will be available upon importing settings module in any other part of the code.
+
+# PI_VERSION. Version of the pi board.
+# possible values :
+#	1 - Kanika's old board
+#	3 - One supplied by Hackathon organizers.
+PI_VERSION = 1 
+
